c8901c046247a602cca346b3fda6b7892c5d1c71
andrey <andrey@gmail.com>
2025-07-18 12:08:24 +0300

    https enables

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import fs from 'fs'
+import path from 'path'
+import os from 'os'
+
+
+function getSSLConfig() {
+  const homedir = os.homedir()
+  const sslDir = path.join(homedir, 'certificates')
+  const sslProjectDir = path.join(sslDir, 'mini-app')
+  
+
+  const keyPath = path.join(sslProjectDir, 'privkey.pem')
+  const certPath = path.join(sslProjectDir, 'cert.pem')
+
+
+console.log("Looking for certificates");
+console.log(keyPath);
+console.log(certPath);
+
+  if (!fs.existsSync(keyPath) || !fs.existsSync(certPath)) {
+    console.warn('SSL certificates not found. HTTPS will be disabled.')
+    return false
+  }
+
+  return {
+    key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(certPath)
+  }
+}
+
+
 // https://vite.dev/config/
 export default defineConfig({
+  server:{
+    https: getSSLConfig()
+  },
   plugins: [vue()],
 })
+
